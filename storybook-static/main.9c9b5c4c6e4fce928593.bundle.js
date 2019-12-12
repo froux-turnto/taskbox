01599ff9cb3018252327e7d4711198c37df540ab
froux <froux@turnto.com>
2019-12-12 13:37:10 -0500

    taskbox UI

---
+++ storybook-static/main.9c9b5c4c6e4fce928593.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(434),c(544),n.exports=c(925)},1:function(n,o){},544:function(n,o,c){"use strict";c.r(o);c(545),c(810),c(922)}},[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.565df66a51995a828fd7efd1fd149968.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d328a6afa3bac15d45c335ecc0468d5d",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "df61c4a45e771e66c6af",
+    "url": "main.css"
+  },
+  {
+    "url": "main.ff01048ddda07ac38ae6.bundle.js"
+  },
+  {
+    "url": "runtime~main.ff01048ddda07ac38ae6.bundle.js"
+  },
+  {
+    "url": "vendors~main.ff01048ddda07ac38ae6.bundle.js"
+  }
+]);
